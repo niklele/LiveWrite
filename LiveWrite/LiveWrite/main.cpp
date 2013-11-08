740fe823cc94d620d69cd9f04ec456527dc16251
Ben Mildenhall <ben@mildenhall.stanford.edu>
2013-11-08 09:12:37 -0800

    Blank Xcode project added Added blank xcode project

---
+++ LiveWrite/LiveWrite/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LiveWrite
+//
+//  Created by Ben Mildenhall on 11/8/13.
+//  Copyright (c) 2013 Ben Mildenhall. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
